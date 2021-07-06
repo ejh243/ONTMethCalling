117c85a2aa8ce141437ebc0eae1a7e1539dc8495
ejh243 <e.j.hannon@exeter.ac.uk>
2021-07-06 22:08:50 +0100

    merge fastq files before alignment

--- Scripts/Alignment.sh
+++ Scripts/Alignment.sh
@@ -1,47 +1,32 @@
 ## align ONT data with minimap
   
-## find fastq files within multiple folders where each folder is a separate flowcell
 
-FQFILES=($(ls ${DATADIR}/*/fastq_pass/*q.gz))
-echo "Number of fastq files found for alignment:"" ""${#FQFILES[@]}"""  
- 
-		
-for f in ${FQFILES[@]};	
-  do
-    basename=$(basename $f)
-	folder=${basename/_pass*q.gz}
-	samplename=${basename/.fastq.gz}
-	  
-	mkdir -p ${ALIGNEDDIR}/${folder}	
-	echo "Aligning"" ${basename}"
-	  
-    minimap2 -a -t 16 -x map-ont ${REFGENOME} ${f} \
-       > ${ALIGNEDDIR}/${folder}/${samplename}.sam \
-       2> ${ALIGNEDDIR}/${folder}/${samplename}.sam.log
-  
-    ## sort aligned file
-    samtools sort -T tmp -o ${ALIGNEDDIR}/${folder}/${samplename}.sorted.bam ${ALIGNEDDIR}/${folder}/${samplename}.sam
-  
-    ## delete sam file
-    rm ${ALIGNEDDIR}/${folder}/${samplename}.sam
-  
-done
-
-## merge aligned files within each folder where a folder represents a flowcell/sample 
-cd ${ALIGNEDDIR}/
-FOLDERS=($(ls ))
+FOLDERS=($(ls ${DATADIR}))
 
 for sample in ${FOLDERS[@]}
 do 
-  BAMFILES=($(ls ${sample}/*.sorted.bam))
+	FQFILES=($(ls ${DATADIR}/${sample}/fastq_pass/*_pass*.fastq.gz))
+    
+	id=$(basename ${FQFILES})
+	id=${id/_pass*.fastq.gz}
+	
+	## Merge into single fastq file for each sample/flowcell
+	cat ${FQFILES[@]} > ${DATADIR}/${sample}/fastq_pass/${id}_merged.fastq.gz
+	  
+	mkdir -p ${ALIGNEDDIR}/${id}	
+	echo "Aligning"" ${f}"
+	  
+    minimap2 -a -t 16 -x map-ont ${REFGENOME} ${DATADIR}/${sample}/fastq_pass/${id}_merged.fastq.gz \
+       > ${ALIGNEDDIR}/${id}/${id}.sam \
+       2> ${ALIGNEDDIR}/${id}/${id}.sam.log
   
-  samtools merge ${sample}/merged.sorted.bam ${BAMFILES[@]}
-  
-  ## index aligned file
-  samtools index ${sample}/merged.sorted.bam
+    ## sort aligned file
+    samtools sort -T tmp -o ${ALIGNEDDIR}/${id}/${id}.sorted.bam ${ALIGNEDDIR}/${id}/${id}.sam
+	
+	## index aligned file
+    samtools index ${ALIGNEDDIR}/${id}/${id}.sorted.bam
 
-  ## delete intermediate bam files
-  rm ${BAMFILES[@]}
+    ## delete sam file
+    rm ${ALIGNEDDIR}/${id}/${id}.sam
+  
 done
-
-  
