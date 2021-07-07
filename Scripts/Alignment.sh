110a7d97ca496e7077bef6c837bbb9f7f606ec3d
ejh243 <e.j.hannon@exeter.ac.uk>
2021-07-07 17:15:55 +0100

    add post alignment filtering

--- Scripts/Alignment.sh
+++ Scripts/Alignment.sh
@@ -23,10 +23,17 @@ do
     ## sort aligned file
     samtools sort -T tmp -o ${ALIGNEDDIR}/${id}/${id}.sorted.bam ${ALIGNEDDIR}/${id}/${id}.sam
 	
+	## filter to mapped reads, of high quality
+	samtools view -F 0x04 -q 30 -b ${ALIGNEDDIR}/${id}/${id}.sorted.bam > ${ALIGNEDDIR}/${id}/${id}.sorted.filtered.bam
+	
+	## filter to primary alignments only
+	samtools view -F 256  -b ${ALIGNEDDIR}/${id}/${id}.sorted.filtered.bam > ${ALIGNEDDIR}/${id}/${id}.sorted.filtered.primary.bam # only primary alignments
+	
 	## index aligned file
-    samtools index ${ALIGNEDDIR}/${id}/${id}.sorted.bam
+    samtools index ${ALIGNEDDIR}/${id}/${id}.sorted.filtered.bam
+	samtools index ${ALIGNEDDIR}/${id}/${id}.sorted.filtered.primary.bam
 
     ## delete sam file
     rm ${ALIGNEDDIR}/${id}/${id}.sam
-  
+	  
 done
